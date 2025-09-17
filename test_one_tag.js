af8a315c4e21fa89e4c072f2166573f64f079ef7
nooraihema <ibrahiemshahat@gmail.com>
2025-09-17 18:02:05 +0300

    Delete test_one_tag.js

--- test_one_tag.js
+++
@@ -1,12 +0,0 @@
-// ~/mybot/rafiq-ai/test_one_tag.js
-import { buildIndexSync, tagToIdx } from "./api/intent_engine.js";
-
-console.log("🧪 بدء اختبار طباعة تاج واحد ...");
-buildIndexSync();
-
-const allTags = Object.keys(tagToIdx);
-if (allTags.length > 0) {
-  console.log("📌 أول تاج كامل:", allTags[0]);
-} else {
-  console.log("⚠️ مفيش أي tags متخزنة.");
-}
