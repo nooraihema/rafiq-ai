785aeefdf728efd03a63c296d9fc9ddd6e8c29d0
nooraihema <ibrahiemshahat@gmail.com>
2025-09-26 13:39:05 +0300

    Update index.js

--- intelligence/linguistic_core/dictionaries/index.js
+++ intelligence/linguistic_core/dictionaries/index.js
@@ -1,19 +1,14 @@
 // intelligence/linguistic_core/dictionaries/index.js
-console.log("--- [CORE] Loading: dictionaries/index.js ---");
 
 import { CONCEPT_MAP } from './concepts.js';
 import { STOP_WORDS } from './stop_words.js';
 import { CAUSAL_PATTERNS } from './patterns.js';
 import { GENERATIVE_LEXICON } from './generative.js';
-import { PREFIXES, SUFFIXES } from './affixes.js';
 
+// نصدر كل القواميس ككائن واحد لتسهيل الوصول إليها من باقي أجزاء المكتبة.
 export const Dictionaries = {
     CONCEPT_MAP,
     STOP_WORDS,
     CAUSAL_PATTERNS,
     GENERATIVE_LEXICON,
-    PREFIXES,
-    SUFFIXES,
 };
-
-console.log("--- [CORE] Loaded: dictionaries/index.js ---");
