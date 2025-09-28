110a849972e83c61af3fa640396d8f1208522103
nooraihema <ibrahiemshahat@gmail.com>
2025-09-28 18:18:49 +0300

    Create anchors.js

---
+++ intelligence/linguistic_core/dictionaries/anchors.js
@@ -0,0 +1,21 @@
+// intelligence/linguistic_core/dictionaries/anchors.js
+
+// المراسي الدلالية: كلمات قوية تفرض حالة مزاجية معينة
+export const ANCHOR_MOODS = {
+    // كلمات احتفالية
+    "مبروك": "celebratory",
+    "نجحت": "celebratory",
+    "إنجاز": "celebratory",
+    
+    // كلمات تمكينية
+    "قررت": "empowering",
+    "هدفي": "empowering",
+    "تحدي": "empowering",
+    
+    // كلمات تأملية
+    "أتساءل": "reflective",
+    "أفكر": "reflective",
+    "أتأمل": "reflective",
+    
+    // يمكنك إضافة المزيد من المراسي هنا
+};
